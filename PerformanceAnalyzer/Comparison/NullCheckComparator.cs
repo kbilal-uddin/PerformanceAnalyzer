a0784eabf0438f96751e4998ae8f84bbc2ca36e2
kbilal-uddin <khbilal.uddin@gmail.com>
2024-05-01 18:01:13 +0500

    Added Is not Null vs !=null check

---
+++ PerformanceAnalyzer/Comparison/NullCheckComparator.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using PerformanceAnalyzer.Models;
+
+namespace PerformanceAnalyzer.Comparison
+{
+    [MemoryDiagnoser]
+    public class NullCheckComparator
+    {
+        Github git = new Github();
+
+        [Benchmark]
+        public void WithTradationalNullCheck()
+        {
+            if(git != null)
+            {
+                git.repo_name = "kbilal-uddin/PerformanceAnalyzer";
+                git.branch_name = "performance-analyzer/add-is-not-null-comparsion";
+            }    
+        }
+
+        [Benchmark]
+        public void WithIsNotNullCheck()
+        {
+            if (git is not null)
+            {
+                git.repo_name = "kbilal-uddin/PerformanceAnalyzer";
+                git.branch_name = "performance-analyzer/add-is-not-null-comparsion";
+            }
+        }
+    }
+}
---
+++ PerformanceAnalyzer/Extensions/CustomConsole.cs
@@ -0,0 +1,14 @@
+﻿namespace PerformanceAnalyzer.Extensions
+{
+    static class CustomConsole
+    {
+        public static void WriteLine(string value, ConsoleColor background = ConsoleColor.Black, ConsoleColor foreground = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.WriteLine(value);
+
+            Console.ResetColor();
+        }
+    }
+}
